438ddf6b2969878c7fca40dce69b7b3c0fcb09ba
Piyush Jain <piyushjz0402@gmail.com>
2023-04-19 11:33:22 +0530

    Added test user

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from "bcrypt"
 
 const userSchema = new mongoose.Schema(
   {
@@ -21,9 +22,32 @@ const userSchema = new mongoose.Schema(
 );
 
 const User = mongoose.model('User', userSchema);
+const hashpass = await bcrypt.hash("pass", 10)
 const testUsers = await User.create([
   {
-    
-  }
+    name: "test1",
+    email: "test1@abc.com",
+    password: hashpass
+  },
+  {
+    name: "test2",
+    email: "test2@abc.com",
+    password: hashpass
+  },
+  {
+    name: "test3",
+    email: "test3@abc.com",
+    password: hashpass
+  },
+  {
+    name: "test4",
+    email: "test4@abc.com",
+    password: hashpass
+  },
+  {
+    name: "test5",
+    email: "test5@abc.com",
+    password: hashpass
+  },
 ])
 export default User;
