4d11b1758c43664390087a447a95823a2f66da6e
Piyush Jain <piyushjz0402@gmail.com>
2023-04-26 18:57:08 +0530

    Added task table in admin dashboard

--- client/src/components/AdminPage/TaskTable.jsx
+++ client/src/components/AdminPage/TaskTable.jsx
@@ -1,31 +1,70 @@
 import { useEffect } from "react";
-import {} from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { getTasks } from "../../redux/fetchSlice";
 
 const TaskTable = () => {
-  const tasks = useSelector((state) => state.taskList.tasks);
+  const { tasks } = useSelector((state) => state.fetch.apiData);
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    console.log(tasks);
+    dispatch(getTasks());
   }, []);
-  return (
-    <div className="overflow-x-auto">
-      <table className="table table-zebra w-full">
-        {/* head */}
-        <thead>
-          <tr>
-            <th>employee email</th>
-            <th>tasks</th>
-          </tr>
-        </thead>
-        <tbody>
-          {/* row 1 */}
-          <tr>
-            <td>Cy Ganderton</td>
-            <td>Quality Control Specialist</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  );
+
+  const getTotalTime = (userEmail) => {
+    const userTasks = tasks[userEmail];
+    userTasks.reduce((total, task) => {
+      if (task.start && task.end) {
+        const timeTaken = new Date(task.end) - new Date(task.start);
+        return total + timeTaken;
+      }
+    }, 0);
+  };
+
+  const renderTable = () => {
+    if (!tasks) {
+      return <h3>No Tasks Present</h3>;
+    }
+    return (
+      <div className="overflow-x-auto">
+        <table className="table table-zebra w-full">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>employee email</th>
+              <th>tasks</th>
+              <th>time taken</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Object.keys(tasks).map((userEmail, index) => {
+              return (
+                <tr key={index}>
+                  <td>{userEmail}</td>
+                  <td>
+                    <select>
+                      <option default disabled>
+                        Select a task
+                      </option>
+                      {tasks[userEmail].map((task) => {
+                        return (
+                          <option key={task._id} value={task.description}>
+                            {task.description}
+                          </option>
+                        );
+                      })}
+                    </select>
+                  </td>
+                  <td>{getTotalTime(userEmail)}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  };
+
+  return <>{renderTable()}</>;
 };
 
 export default TaskTable;
--- client/src/redux/fetchSlice.js
+++ client/src/redux/fetchSlice.js
@@ -2,53 +2,44 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const initialState = {
-    apiData: [],
-    loading: true,
-    success: false,
-    error: "No errors"
-}
+  apiData: [],
+  loading: true,
+  success: false,
+  error: "No errors",
+};
 
-let auth = localStorage.getItem('auth');
+let auth = localStorage.getItem("auth");
 
-export const getTasks = createAsyncThunk('/getTasks', async () => {
-    const res = await axios('http://localhost:3001/api/tasks', {
-        headers: {
-            Authorization: `Bearer ${auth}`
-        }
-    });
-    console.log(res.data);
-    return res;
-})
-
+export const getTasks = createAsyncThunk("/getTasks", async () => {
+  const res = await axios("http://localhost:3001/api/tasks", {
+    headers: {
+      Authorization: `Bearer ${auth}`,
+    },
+  });
+  // console.log(res.data);
+  return res;
+});
 
 const fetchSlice = createSlice({
-    name: fetch,
-    initialState,
-    reducers: {},
-    extraReducers: {
-        [getTasks.pending]: (state) => {
-            state
-        },
-        [getTasks.fulfilled]: (state, action) => {
-            console.log(action);
-            state.loading = false
-            state.success = true
-            state.apiData = action.payload.data
-        },
-        [getTasks.rejected]: (state, action) => {
-            state.loading = true
-            state.success = false
-            state.apiData = action.payload
-        }
-    }
-})
-
+  name: fetch,
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [getTasks.pending]: (state) => {
+      state;
+    },
+    [getTasks.fulfilled]: (state, action) => {
+      // console.log(action);
+      state.loading = false;
+      state.success = true;
+      state.apiData = action.payload.data;
+    },
+    [getTasks.rejected]: (state, action) => {
+      state.loading = true;
+      state.success = false;
+      state.apiData = action.payload;
+    },
+  },
+});
 
 export default fetchSlice.reducer;
-
-
-
-
-
-
-
