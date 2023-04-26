d3b431f88a4df3de484491461aa8c5dc3c0f4fbb
vishesh-baj <vishesh.bajpayee@icloud.com>
2023-04-26 12:29:06 +0530

    added reverse auth function

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,12 +9,15 @@ import { PATHS } from "./router/paths";
 import Task from "./components/Task";
 import { isAuthenticated } from "./utils/auth";
 import ProtectedRoute from "./router/ProtectedRoute";
+import Navbar from "./components/navbar/Navbar";
 const App = () => {
   const { isLoggedIn, user } = useSelector((state) => state.auth);
 
   return (
     <Routes>
       <Route path={PATHS.root} element={<Login />} />
+      <Route path={PATHS.taskList} element={<TaskList />} />
+      <Route path={PATHS.createTasks} element={<CreateTask />} />
     </Routes>
   );
 };
---
+++ client/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
+const Sidebar = () => {
+  return <div>
+    
+  </div>;
+};
+
+export default Sidebar;
