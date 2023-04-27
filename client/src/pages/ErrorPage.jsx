4c5af5d1bf4955339cd62c2e769623d05f572d86
vishesh-baj <vishesh.bajpayee@icloud.com>
2023-04-27 12:56:54 +0530

    added error page

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -1,7 +1,25 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import { GrHomeRounded } from "react-icons/gr";
+import { PATHS } from "../router/paths";
 const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  return (
+    <div className="w-screen h-screen flex justify-center items-center bg-neutral-100">
+      <div className="w-1/2 h-1/2 bg-neutral-300 rounded-xl shadow-lg">
+        <div className="flex flex-col justify-center items-center w-full h-full gap-4 p-4">
+          <h1 className="text-4xl md:text-5xl lg:text-8xl">404 Error</h1>
+          <p className="text-lg md:text-2xl lg:text-4xl">Page doesnot exist</p>
+          <Link
+            className="btn w-[4rem] btn-info outline-none px-4 py-2 flex  gap-4 "
+            to={PATHS.createTasks}
+          >
+            <GrHomeRounded size={20} />
+          </Link>
+          <span>click to go back to home</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ErrorPage;
