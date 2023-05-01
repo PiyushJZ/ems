d0430a6c54dec6bd7e7ba55089cbde4ad90497ed
anshviyogitophat <diptanshu.tophat@gmail.com>
2023-05-01 14:31:12 +0530

    verify token updation

--- server/routes/notes.js
+++ server/routes/notes.js
@@ -1,10 +1,11 @@
 import express from 'express'
 const router = express.Router()
 import {deleteNote, getNotes,postNotes,updateNote} from "../controllers/notes.js"
+import { verifyAccessToken } from '../middlewares/index.js'
 
-router.get('/', getNotes)
-router.post("/",postNotes)
-router.put("/:noteId", updateNote)
-router.delete("/:noteId", deleteNote)
+router.get('/',verifyAccessToken, getNotes)
+router.post("/",verifyAccessToken,postNotes)
+router.put("/:noteId",verifyAccessToken, updateNote)
+router.delete("/:noteId",verifyAccessToken, deleteNote)
 
 export default router;
