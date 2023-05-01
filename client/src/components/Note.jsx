11da07bb2f437eb1c540b6a3b44d9bdb905fa62a
rahulbirla <rahul.tophat@gmail.com>
2023-05-01 16:20:41 +0530

    task delete functionlity is added

--- client/src/components/Note.jsx
+++ client/src/components/Note.jsx
@@ -1,8 +1,46 @@
 import React from "react";
+import { FETCH_WRAPPER } from "../api";
+import { updateNote } from "../redux/fetchSlice";
+import { useSelector , useDispatch } from "react-redux";
 
-const Note = () => {
+const Note = ({ val }) => {
+  const { title, createdUser, description, createdAt } = val;
+
+  const { notes } = useSelector((store) => store.fetch);
+  const dispatch = useDispatch();
+
+  console.log(notes);
+
+  const handleDelete = async (id) => {
+    try {
+      const token = localStorage.getItem("authToken");
+      const response = await FETCH_WRAPPER.delete(`notes/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (response) {
+        const filteredNotes = notes.filter((v) => v._id !== id);
+        dispatch(updateNote(filteredNotes));
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <div className="min-w-[20vw] h-[50vh]  rounded-xl bg-base-200 shadow-lg hover:shadow-2xl transition ease-in duration-200 cursor-pointer hover:scale-105"></div>
+    <div className="min-w-[20vw] h-[50vh]  rounded-xl bg-base-200 shadow-lg hover:shadow-2xl transition ease-in duration-200 cursor-pointer hover:scale-105">
+      <h4>{title}</h4>
+      <h4>{createdUser}</h4>
+      <h4>{description}</h4>
+      <h4>{createdAt}</h4>
+      <br />
+      <br />
+      <button>Edit</button>
+      <br />
+      <br />
+      <button onClick={() => handleDelete(val._id)}>Delete</button>
+    </div>
   );
 };
 
--- client/src/redux/fetchSlice.js
+++ client/src/redux/fetchSlice.js
@@ -3,11 +3,29 @@ import { FETCH_WRAPPER } from '../api';
 
 const initialState = {
   tasks: [],
+  notes: [],
   loading: true,
   success: false,
   error: 'No error',
 };
 
+// fetching notes
+export const getNotes = createAsyncThunk("/getNotes", async () => {
+  try {
+    const token = localStorage.getItem("authToken");
+    const response = await FETCH_WRAPPER.get("notes", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    console.log(response);
+    return response.data.tasks;
+  } catch (err) {
+    return err.response.statusText;
+  }
+});
+
+// fetching Tasks
 export const getTasks = createAsyncThunk('/getTasks', async () => {
   try {
     const token = localStorage.getItem('authToken');
@@ -24,6 +42,7 @@ export const getTasks = createAsyncThunk('/getTasks', async () => {
 
 export const clearList = createAction('clearList');
 export const updateList = createAction('updateList');
+export const updateNote = createAction('updateNote');
 
 const fetchSlice = createSlice({
   name: 'fetch',
@@ -57,6 +76,28 @@ const fetchSlice = createSlice({
       state.success = true;
       state.error = 'No error';
       state['tasks'] = action.payload;
+    });
+    builder.addCase(getNotes.pending, (state) => {
+      state.loading = true;
+      state.success = false;
+      state.error = 'No error';
+    });
+    builder.addCase(getNotes.fulfilled, (state, action) => {
+      state.loading = false;
+      state.success = true;
+      state.error = 'No error';
+      state.notes = action.payload;
+    });
+    builder.addCase(getNotes.rejected, (state, action) => {
+      state.loading = false;
+      state.success = false;
+      state.error = action.payload;
+    });
+    builder.addCase("updateNote", (state, action) => {
+      console.log(action.payload);
+      state.loading = false;
+      state.success = true;
+      state.notes = action.payload;
     });
   },
 });
