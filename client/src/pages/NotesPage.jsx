1e0409240e23882cd471940f5abffeeacf0b019e
vishesh-baj <vishesh.bajpayee@icloud.com>
2023-05-01 12:37:02 +0530

    added form for creating notes

--- client/src/pages/NotesPage.jsx
+++ client/src/pages/NotesPage.jsx
@@ -1,25 +1,51 @@
 import React from "react";
-
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 const NotesPage = () => {
+  const notesSchema = yup.object({
+    title: yup
+      .string()
+      .max(25, "Title cannot be more than 25 characters!")
+      .required("Title cannot be an empty string"),
+    description: yup.string().required("Description cannot be an empty field!"),
+  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(notesSchema),
+  });
+  const onSubmit = (data) => console.log("NOTES DATA: ", data);
   return (
     <>
-      <div className="w-screen h-screen p-8 flex flex-col gap-4 justify-center items-center">
-        <div>
-          <h1 className="text-xl md:text-3xl lg:text-4xl text-primary mb-8">
+      <div className="w-screen h-screen p-8">
+        <form
+          className="w-full h-full flex flex-col gap-4 justify-center items-center"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <h1 className="text-xl md:text-3xl lg:text-4xl font-semibold text-info mb-8 text-center">
             Create tasks
           </h1>
           <input
+            {...register("title")}
+            name="title"
             placeholder="enter title"
-            className="input input-primary w-1/4"
+            className="input input-info w-1/4"
             type="text"
           />
+          <p className="text-rose-500">{errors.title?.message}</p>
           <input
+            {...register("description")}
+            name="description"
             placeholder="enter description"
-            className="input input-primary w-1/4"
+            className="input input-info w-1/4"
             type="text"
           />
+          <p className="text-rose-500">{errors.description?.message}</p>
           <button className="btn btn-info w-1/4">Submit</button>
-        </div>
+        </form>
       </div>
     </>
   );
