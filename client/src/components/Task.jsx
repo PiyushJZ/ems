c38742c222eb8e9deb75aed41948ae99096e551f
Piyush Jain <piyushjz0402@gmail.com>
2023-05-03 14:14:22 +0530

    Edit bug fixed

--- client/src/components/Task.jsx
+++ client/src/components/Task.jsx
@@ -1,16 +1,16 @@
-import { useState } from "react";
-import { GrEdit, GrTrash } from "react-icons/gr";
-import { useDispatch, useSelector } from "react-redux";
-import { getTasks, updateList } from "../redux/fetchSlice";
-import { FETCH_WRAPPER } from "../api";
-import Timer from "./Timer";
-import Swal from "sweetalert2";
+import { useState } from 'react';
+import { GrEdit, GrTrash } from 'react-icons/gr';
+import { useDispatch, useSelector } from 'react-redux';
+import { getTasks, updateList } from '../redux/fetchSlice';
+import { FETCH_WRAPPER } from '../api';
+import Timer from './Timer';
+import Swal from 'sweetalert2';
 
 function Task({ description, id, start, end, index }) {
   const { tasks } = useSelector((state) => state.fetch);
   const [isEdit, setIsEdit] = useState(false);
   const [desc, setDesc] = useState(description);
-  const accessType = localStorage.getItem("accessType");
+  const accessType = localStorage.getItem('accessType');
   const dispatch = useDispatch();
 
   // Edit the task
@@ -20,13 +20,13 @@ function Task({ description, id, start, end, index }) {
     };
     const response = await FETCH_WRAPPER.put(`tasks/${id}`, data, {
       headers: {
-        Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+        Authorization: `Bearer ${localStorage.getItem('authToken')}`,
       },
     });
     if (response.data.status === true) {
       setIsEdit(!isEdit);
     } else {
-      alert("Task description not changed");
+      alert('Task description not changed');
     }
   }
 
@@ -39,27 +39,27 @@ function Task({ description, id, start, end, index }) {
       showCancelButton: true,
       confirmButtonColor: '#3085d6',
       cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes, delete it!'
-  }).then(async(result) => {
+      confirmButtonText: 'Yes, delete it!',
+    }).then(async (result) => {
       if (result.value) {
         const response = await FETCH_WRAPPER.delete(`tasks/${id}`, {
           headers: {
-            Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+            Authorization: `Bearer ${localStorage.getItem('authToken')}`,
           },
         });
         if (response.status === 200) {
-          if (accessType === "admin") {
+          if (accessType === 'admin') {
             dispatch(updateList(tasks.filter((task) => task._id !== id)));
           } else {
             dispatch(getTasks());
           }
           Swal.fire({
-            icon: "success",
-            title: "task deleted successfully",
+            icon: 'success',
+            title: 'task deleted successfully',
           });
         }
       } else {
-        Swal("Task is not deleted");
+        Swal('Task is not deleted');
       }
     });
   }
@@ -72,7 +72,7 @@ function Task({ description, id, start, end, index }) {
     };
     const response = await FETCH_WRAPPER.put(`tasks/${id}`, data, {
       headers: {
-        Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+        Authorization: `Bearer ${localStorage.getItem('authToken')}`,
       },
     });
     if (response.data.status === true) {
@@ -83,7 +83,7 @@ function Task({ description, id, start, end, index }) {
   // End the task
   async function endTask() {
     if (!start) {
-      alert("NOT ALLOWED");
+      alert('NOT ALLOWED');
       return;
     }
     const end = Date.now();
@@ -92,7 +92,7 @@ function Task({ description, id, start, end, index }) {
     };
     const response = await FETCH_WRAPPER.put(`tasks/${id}`, data, {
       headers: {
-        Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+        Authorization: `Bearer ${localStorage.getItem('authToken')}`,
       },
     });
 
@@ -103,19 +103,19 @@ function Task({ description, id, start, end, index }) {
 
   return (
     <tr>
-      <th className="w-2">{index + 1}</th>
+      <th className='w-2'>{index + 1}</th>
       {isEdit ? (
         <>
-          <td className="w-10 max-w-[200px] overflow-auto whitespace-nowrap">
+          <td className='w-10 max-w-[200px] overflow-auto whitespace-nowrap'>
             <input
-              className="input max-w-[200px] relative input-bordered input-sm"
-              type="text"
+              className='input max-w-[200px] relative input-bordered input-sm'
+              type='text'
               value={desc}
               onChange={(e) => setDesc(e.target.value)}
             />
             <button
               onClick={editTask}
-              className="btn border-2 btn-info btn-sm absolute mx-2"
+              className='btn border-2 btn-info btn-sm absolute mx-2'
             >
               Change Description
             </button>
@@ -123,61 +123,86 @@ function Task({ description, id, start, end, index }) {
         </>
       ) : (
         <>
-          <td className="w-10 max-w-[200px] relative overflow-auto whitespace-nowrap">
+          <td className='w-10 max-w-[200px] relative overflow-auto whitespace-nowrap'>
             {desc}
           </td>
         </>
       )}
-      <td className="w-20">
-        {!start && !end ? "Not Yet Started" : ""}
-        {start ? <Timer start={start} end={end} /> : ""}
+      <td className='w-20'>
+        {!start && !end ? 'Not Yet Started' : ''}
+        {start ? (
+          <Timer
+            start={start}
+            end={end}
+          />
+        ) : (
+          ''
+        )}
       </td>
       {/* new Date column added */}
-      <td className="w-20">
-        {!end ? "Task not Completed" : ""}
-        {start ? <Timer end={end} /> : ""}
+      <td className='w-20'>
+        {!end ? 'Task not Completed' : ''}
+        {start ? <Timer end={end} /> : ''}
       </td>
       {/* new Date column ended */}
-      {accessType === "employee" ? (
-        <td className="w-10">
-          {start && end ? "Task Completed" : ""}
+      {accessType === 'employee' ? (
+        <td className='w-10'>
+          {start && end ? 'Task Completed' : ''}
           {start && !end ? (
-            <div className="flex gap-4">
-              <button disabled className="btn btn-info btn-sm">
+            <div className='flex gap-4'>
+              <button
+                disabled
+                className='btn btn-info btn-sm'
+              >
                 Start
               </button>
-              <button className="btn btn-error btn-sm" onClick={endTask}>
+              <button
+                className='btn btn-error btn-sm'
+                onClick={endTask}
+              >
                 Stop
               </button>
             </div>
           ) : (
-            ""
+            ''
           )}
           {!start && !end ? (
-            <div className="flex gap-4">
-              <button className="btn btn-success btn-sm" onClick={startTask}>
+            <div className='flex gap-4'>
+              <button
+                className='btn btn-success btn-sm'
+                onClick={startTask}
+              >
                 Start
               </button>
-              <button className="btn btn-warning btn-sm" onClick={endTask}>
+              <button
+                className='btn btn-warning btn-sm'
+                onClick={endTask}
+              >
                 Stop
               </button>
             </div>
           ) : (
-            ""
+            ''
           )}
         </td>
       ) : (
-        ""
+        ''
       )}
 
-      <td className="w-10">
+      <td className='w-10'>
         <button
-          className="btn btn-info btn-sm"
-          onClick={() => setIsEdit(!isEdit)}
+          className='btn btn-info btn-sm'
+          onClick={() => {
+            setIsEdit(!isEdit);
+            setDesc(description);
+          }}
         >
           <GrEdit />
         </button>
-        <button className="btn btn-error ml-4 btn-sm" onClick={deleteTask}>
+        <button
+          className='btn btn-error ml-4 btn-sm'
+          onClick={deleteTask}
+        >
           <GrTrash />
         </button>
       </td>
