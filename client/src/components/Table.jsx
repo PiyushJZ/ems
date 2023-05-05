f35466d8e2c53830c31a23948e23c21604811d1a
Piyush Jain <piyushjz0402@gmail.com>
2023-05-05 15:33:15 +0530

    Updated the Table component to be used in multiple pages

--- client/src/components/Table.jsx
+++ client/src/components/Table.jsx
@@ -1,7 +1,41 @@
+import { ImListNumbered } from 'react-icons/im';
+import { MdAssignmentAdd } from 'react-icons/md';
+import { BsCalendarDate } from 'react-icons/bs';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { updateList } from '../redux/fetchSlice';
+import { PATHS } from '../router/paths';
 import Task from './Task';
 
 const Table = ({ type, tasks }) => {
   const accessType = localStorage.getItem('accessType');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const renderTime = (time) => {
+    if (isNaN(time)) {
+      return;
+    }
+    return `${parseInt(time / 60 / 60)} : ${parseInt(time / 60) % 60} : ${
+      parseInt(time) % 60
+    }`;
+  };
+
+  const getTotalTime = (userEmail) => {
+    try {
+      const userTasks = tasks[userEmail];
+      const totalTime = userTasks.reduce((total, task) => {
+        if (task.start && task.end) {
+          const timeTaken = (new Date(task.end) - new Date(task.start)) / 1000;
+          return total + timeTaken;
+        }
+        return total;
+      }, 0);
+      return renderTime(totalTime);
+    } catch (err) {
+      return '';
+    }
+  };
 
   return (
     <div className='overflow-x-auto'>
@@ -19,30 +53,74 @@ const Table = ({ type, tasks }) => {
             </tr>
           ) : (
             <tr>
-              <th></th>
+              <th>S no.</th>
               <th>employee email</th>
               <th>tasks</th>
+              <th>Attendance</th>
+              <th>Task Assignment</th>
               <th>time taken</th>
             </tr>
           )}
         </thead>
         {/* Table Body */}
         <tbody>
-          {type === 'employee'
-            ? tasks.map((task, index) => {
-                return (
-                  <Task
-                    key={task._id}
-                    index={index}
-                    description={task.description}
-                    status={task.status}
-                    id={task._id}
-                    start={task?.start}
-                    end={task?.end}
-                  />
-                );
-              })
-            : ''}
+          <>
+            {type === 'employee'
+              ? tasks.map((task, index) => {
+                  return (
+                    <Task
+                      key={task._id}
+                      index={index}
+                      description={task.description}
+                      status={task.status}
+                      id={task._id}
+                      start={task?.start}
+                      end={task?.end}
+                    />
+                  );
+                })
+              : ''}
+          </>
+          <>
+            {type === 'admin'
+              ? Object.keys(tasks).map((userEmail, index) => {
+                  return (
+                    <tr key={index}>
+                      <td>{index + 1}</td>
+                      <td>{userEmail}</td>
+                      <td>
+                        <button
+                          onClick={() => {
+                            dispatch(updateList(tasks[userEmail]));
+                            return navigate(PATHS.adminPage + PATHS.taskList);
+                          }}
+                          className='btn btn-accent btn-sm'
+                        >
+                          <ImListNumbered />
+                        </button>
+                      </td>
+                      <td>
+                        <button className='btn btn-accent btn-sm'>
+                          <BsCalendarDate />
+                        </button>
+                      </td>
+                      <td>
+                        <button
+                          className='btn btn-accent btn-sm'
+                          onClick={() => (
+                            localStorage.setItem('assignTask', userEmail),
+                            navigate(PATHS.createTasks)
+                          )}
+                        >
+                          <MdAssignmentAdd />
+                        </button>
+                      </td>
+                      <td>{getTotalTime(userEmail)}</td>
+                    </tr>
+                  );
+                })
+              : ''}
+          </>
         </tbody>
       </table>
     </div>
--- client/src/pages/AdminPage.jsx
+++ client/src/pages/AdminPage.jsx
@@ -1,102 +1,24 @@
-import React from "react";
-import { useEffect } from "react";
-import { ImListNumbered } from "react-icons/im";
-import { MdAssignmentAdd } from "react-icons/md";
-import { BsCalendarDate } from "react-icons/bs";
-import { useSelector, useDispatch } from "react-redux";
-import { getTasks, updateList } from "../redux/fetchSlice";
-import { PATHS } from "../router/paths";
-import { useNavigate } from "react-router-dom";
+import React from 'react';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getTasks } from '../redux/fetchSlice';
+import Table from '../components/Table';
 
 const AdminPage = () => {
   const { tasks } = useSelector((state) => state.fetch);
   const dispatch = useDispatch();
-  const navigate = useNavigate();
-  console.log("TASKS: ", tasks);
 
   useEffect(() => {
     dispatch(getTasks());
   }, []);
 
-  const renderTime = (time) => {
-    if (isNaN(time)) {
-      return;
-    }
-    return `${parseInt(time / 60 / 60)} : ${parseInt(time / 60) % 60} : ${
-      parseInt(time) % 60
-    }`;
-  };
-
-  const getTotalTime = (userEmail) => {
-    try {
-      const userTasks = tasks[userEmail];
-      const totalTime = userTasks.reduce((total, task) => {
-        if (task.start && task.end) {
-          const timeTaken = (new Date(task.end) - new Date(task.start)) / 1000;
-          return total + timeTaken;
-        }
-        return total;
-      }, 0);
-      return renderTime(totalTime);
-    } catch (err) {
-      return "";
-    }
-  };
-
   const renderTable = () => {
     return (
-      <div className="overflow-x-auto">
-        <table className="table table-zebra w-full">
-          {/* head */}
-          <thead>
-            <tr>
-              <th>S no.</th>
-              <th>employee email</th>
-              <th>tasks</th>
-              <th>Attendance</th>
-              <th>Task Assignment</th>
-              <th>time taken</th>
-            </tr>
-          </thead>
-          <tbody>
-            {Object.keys(tasks).map((userEmail, index) => {
-              return (
-                <tr key={index}>
-                  <td>{index + 1}</td>
-                  <td>{userEmail}</td>
-                  <td>
-                    <button
-                      onClick={() => {
-                        dispatch(updateList(tasks[userEmail]));
-                        return navigate(PATHS.adminPage + PATHS.taskList);
-                      }}
-                      className="btn btn-accent btn-sm"
-                    >
-                      <ImListNumbered />
-                    </button>
-                  </td>
-                  <td>
-                    <button className="btn btn-accent btn-sm">
-                      <BsCalendarDate />
-                    </button>
-                  </td>
-                  <td>
-                    <button
-                      className="btn btn-accent btn-sm"
-                      onClick={() => (
-                        localStorage.setItem("assignTask", userEmail),
-                        navigate(PATHS.createTasks)
-                      )}
-                    >
-                      <MdAssignmentAdd />
-                    </button>
-                  </td>
-                  <td>{getTotalTime(userEmail)}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+      <div>
+        <Table
+          type={'admin'}
+          tasks={tasks}
+        />
       </div>
     );
   };
