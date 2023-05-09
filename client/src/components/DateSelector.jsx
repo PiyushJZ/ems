5b3783989d08e336ff4594ef4f0ae7d55e711e20
Piyush Jain <piyushjz0402@gmail.com>
2023-05-09 18:15:29 +0530

    testing git issue

--- client/src/components/DateSelector.jsx
+++ client/src/components/DateSelector.jsx
@@ -1,12 +1,10 @@
-import { useEffect, useState } from "react";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+import { useEffect, useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 const DateSelector = ({ selectedDate }) => {
   const [date, setDate] = useState(new Date());
 
-  console.log(date);
-  
   useEffect(() => {
     selectedDate(date.toISOString().slice(0, 10));
   }, []);
@@ -14,21 +12,21 @@ const DateSelector = ({ selectedDate }) => {
   return (
     <DatePicker
       selected={date}
-      onChange={(d) => {
+      onChange={d => {
         setDate(d);
         selectedDate(d.toISOString().slice(0, 10));
       }}
       // showIcon
       required
-      title="Select Date"
-      dateFormat="dd/MM/yyyy"
-      placeholderText="dd/MM/yyyy"
-      dropdownMode="scroll"
+      title='Select Date'
+      dateFormat='dd/MM/yyyy'
+      placeholderText='dd/MM/yyyy'
+      dropdownMode='scroll'
       maxDate={new Date()}
       openToDate={new Date()}
       isClearable={false}
-      className="input-info input w-full"
-      form="attendance-form"
+      className='input-info input w-full'
+      form='attendance-form'
     />
   );
 };
