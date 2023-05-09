9f4f6ebde29581f717233afcbacdf1996aaa2b77
Piyush Jain <piyushjz0402@gmail.com>
2023-05-09 08:49:46 +0530

    Atendance module - switched component library

--- client/src/components/Attendance.jsx
+++ client/src/components/Attendance.jsx
@@ -1,22 +1,33 @@
 import React, { useEffect, useState } from 'react';
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
+import { Calendar, DateObject } from 'react-multi-date-picker';
+import DatePicker from 'react-multi-date-picker';
+import 'react-multi-date-picker/styles/layouts/mobile.css';
 
 const Attendance = ({ attendance }) => {
-  const [dates, setDates] = useState([new Date()]);
-  console.log(dates);
+  const [dates, setDates] = useState(new DateObject());
+
   useEffect(() => {
-    attendance.map((item) => {
-      setDates([...dates, new Date(item.date)]);
+    const temp = [];
+    attendance.map(item => {
+      temp.push(new DateObject(item.date));
     });
+    setDates(temp);
   }, []);
 
   return (
     <>
       <div>
         <Calendar
-          activeStartDate={new Date()}
-          defaultValue={dates}
+          value={dates}
+          showOtherDays={true}
+          // disabled={true}
+          months={new DateObject().months.map(month => month.shortName)}
+          // disableDayPicker={true}
+          // onChange={}
+          monthYearSeparator={'|'}
+          highlightToday={false}
+          maxDate={new DateObject()}
+          className='rmdp-mobile'
         />
       </div>
     </>
